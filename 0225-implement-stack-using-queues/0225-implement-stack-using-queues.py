fe3d22e805862aceed8fa097bdf5eb11659c4fdc
Arnab Sarkar <sarkararnab088@gmail.com>
2026-08-31 22:22:44 +0530

    Time: 0 ms (100%), Space: 19.6 MB (18%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,40 @@
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.queue.append(x)
+        # Rotate the queue to make the newly added element appear at the front
+        for i in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Return whether the stack is empty.
+        """
+        return not self.queue
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
